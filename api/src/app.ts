e4426bc31fe50acabad12ef97b6014db93825acf
Stefan Fischer <stefischer@mail.com>
2022-12-21 17:48:11 -0500

    Create app file for initializing express application

---
+++ api/src/app.ts
@@ -0,0 +1,23 @@
+import express, { Application } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import { placeholderRoutes } from './routes/index.js';
+
+function initializeApp(): Application {
+    const app = express();
+
+    app.use(morgan("dev"));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cors());
+    app.set("trust proxy", true);
+    app.set("trust proxy", "loopback");
+
+    /* Routes */
+    app.use("/placeholder", placeholderRoutes);
+
+    return app;
+};
+
+export default initializeApp;
