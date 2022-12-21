266be7f1812e8d65be51e412d5a68353df662097
Stefan Fischer <stefischer@mail.com>
2022-12-21 18:40:13 -0500

    Add pagination middleware

---
+++ api/src/middleware/extractPaginationParams.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { DEFAULTS } from '../constants/index.js';
+
+function extractPaginationParams(req: Request, res: Response, next: NextFunction) {
+
+    const page: number = typeof req.query.page === "string" ? parseInt(req.query.page, 10) : 1;
+    const limit: number = DEFAULTS.LIMIT;
+    const offset: number = limit * (page - 1);
+
+    res.locals.page = page;
+    res.locals.limit = limit;
+    res.locals.offset = offset;
+
+    next();
+};
+
+export default extractPaginationParams;
