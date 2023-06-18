610e7da0e920724e2de815fa61d7b2db4e03a812
Stefan Fischer <stefischer@mail.com>
2023-06-18 02:34:38 -0400

    Update extractPaginationParams middleware

--- api/src/middleware/extractPaginationParams.ts
+++ api/src/middleware/extractPaginationParams.ts
@@ -1,15 +1,15 @@
-import { Request, Response, NextFunction } from 'express';
-import { DEFAULTS } from '../constants/index.js';
+import { Request, Response, NextFunction } from '@@types/express.js';
+import { DEFAULTS } from '@@constants/index.js';
 
-function extractPaginationParams(req: Request, res: Response, next: NextFunction) {
+function extractPaginationParams(req: Request<any>, res: Response, next: NextFunction) {
 
     const page: number = typeof req.query.page === "string" ? parseInt(req.query.page, 10) : 1;
     const limit: number = DEFAULTS.LIMIT;
     const offset: number = limit * (page - 1);
 
-    res.locals.page = page;
-    res.locals.limit = limit;
-    res.locals.offset = offset;
+    res.locals.pagination = {
+        page, limit, offset
+    };
 
     next();
 };
