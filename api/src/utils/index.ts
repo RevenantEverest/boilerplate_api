4c2cd9fe1780deaf9b18b02ff8f69950707cfad8
Stefan Fischer <stefischer@mail.com>
2023-06-18 02:35:39 -0400

    Add errors to utils index

--- api/src/utils/index.ts
+++ api/src/utils/index.ts
@@ -1,5 +1,6 @@
 export * as colors from './colors.js';
 export * as common from './common.js';
 export * as entities from './entities.js';
+export * as errors from './errors.js';
 export * as logs from './logs.js';
 export * as promises from './promises.js';
